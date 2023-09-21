d8d9c58e40ea99593e2a07aa4c8071c22c4c6532
AlexioPsh <79427668+AlexioPsh@users.noreply.github.com>
2023-09-21 17:26:00 +0100

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeMoveComponent = document.querySelector(".mainpageButton");
+    fadeMoveComponent.classList.add("loaded");
+    fadeMoveComponent = document.querySelector(".info");
+    fadeMoveComponent.classList.add("loaded");
+});
